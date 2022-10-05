40c20151d262139347c41b197ac3f5fb6f21a580
Bastinanub <94133079+Bastinanub@users.noreply.github.com>
2022-10-05 19:38:46 +0530

    Create 47 calculate quotient and reminder.c

---
+++ 47 calculate quotient and reminder.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1;
+    int num2;
+    int result;
+    printf("enter two numbers: ");
+    scanf("%d %d",&num1,&num2);
+    result = num1/num2;
+    printf("%d/%d is %d",num1,num2,result);
+    result = num1%num2;
+    printf(" with a reminder of :%d\n",result);
+    return 0;
+}
