b314746fe81de84cf74cd235df0a343e2036649e
Bastinanub <94133079+Bastinanub@users.noreply.github.com>
2022-10-12 10:46:16 +0530

    Update 51 check an integer is odd or even.c

--- 51 check an integer is odd or even.c
+++ 51 check an integer is odd or even.c
@@ -2,11 +2,10 @@
 
 #include <stdio.h>
 int main() {
+    printf("Register number: RA2211042010007\n");
     int number;
     printf("Enter an integer: ");
     scanf("%d", &number);
-
-    // True if the remainder is 0
     if  (number%2 == 0) {
         printf("%d is an even integer.",number);
     }
