4edda347e1bdefb39389d9e436d2ef276dec992e
Bastinanub <94133079+Bastinanub@users.noreply.github.com>
2022-10-12 09:45:48 +0530

    Update 06 declaring an variable(int).c

--- 06 declaring an variable(int).c
+++ 06 declaring an variable(int).c
@@ -3,7 +3,7 @@
 int main() 
 {
   int myNum = 5;
-  printf("Register number: RA2211042010007");
+  printf("Register number: RA2211042010007\n");
   printf("My favorite number is: %d", myNum);
 
   return 0;
