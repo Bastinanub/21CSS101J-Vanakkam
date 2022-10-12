9d8d2d05f9aec3083d4907f1e75d58e943832cf3
Bastinanub <94133079+Bastinanub@users.noreply.github.com>
2022-10-12 10:42:36 +0530

    Update 48 printing rightmost digit of an integer.c

--- 48 printing rightmost digit of an integer.c
+++ 48 printing rightmost digit of an integer.c
@@ -2,11 +2,11 @@
 
 int main() 
 {
+    printf("Register number: RA2211042010007\n");
     int num1;
     int rightdigit;
     printf("enter the number: ");
     scanf("%d",&num1);
     rightdigit = num1%10;
     printf("\nthe right most digit is: %d",rightdigit);
-    return 0;
 }
