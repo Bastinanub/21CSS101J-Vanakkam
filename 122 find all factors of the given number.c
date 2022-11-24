ca36829990c9462529daf093fd309cb244935790
Bastinanub <94133079+Bastinanub@users.noreply.github.com>
2022-11-25 02:28:54 +0530

    Create 122 find all factors of the given number.c

---
+++ 122 find all factors of the given number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Register number: RA2211042010007\n\n");
+    int i, num;
+
+    /* Input number from user */
+    printf("Enter any number to find its factor: ");
+    scanf("%d", &num);
+
+    printf("All factors of %d are: \n", num);
+
+    /* Iterate from 1 to num */
+    for(i=1; i<=num; i++)
+    {
+        /* 
+         * If num is exactly divisible by i
+         * Then i is a factor of num
+         */
+        if(num % i == 0)
+        {
+            printf("%d, ",i);
+        }
+    }
+
+    return 0;
+}
